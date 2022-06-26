4ff3c032f0f68140aa773fece9a46fa59c2dca29
danilo borges <danilosilva2017@hotmail.com>
2022-06-26 19:55:32 -0300

    Métodos em Objetos

--- Aula_03/objetos.js
+++ Aula_03/objetos.js
@@ -8,17 +8,17 @@ let sum = function (a, b)
     return a + b;
 }
 
-let mayObject = 
+let mayObject =
 {
     prop_01: mayFunc(),
     prop_02: "Silva Borges",
-    prop_03: sum(10,10)
+    prop_03: sum(10, 10)
 }
 
 console.log(mayObject.prop_01 + " " + mayObject.prop_02);
 console.log(mayObject.prop_03);
 
-let objePerson = 
+let ObjePerson =
 {
     Name: "Danilo",
     Age: 29,
@@ -26,7 +26,30 @@ let objePerson =
     Height: 1.87
 }
 
-console.log(objePerson.Name);
-console.log(objePerson.Height);
+ObjePerson.Color = "White";
+
+console.log(ObjePerson.Name);
+console.log(ObjePerson.Height);
+console.log(ObjePerson.Color);
+
+console.log(mayObject.prop_03);
+
+mayObject.NomeCompleto = function () 
+{
+    return mayObject.prop_01 + " " + mayObject.prop_02;
+}
+
+console.log(mayObject.NomeCompleto());
 
 
+let MayObj =
+{
+
+}
+
+MayObj.Nome = function ()
+{
+    return "Danilo";
+}
+
+console.log(MayObj.Nome());
