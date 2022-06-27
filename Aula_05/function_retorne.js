1e4d8a6b0a1ef778e74fe4ee5f15189c5777d8d1
danilo borges <danilosilva2017@hotmail.com>
2022-06-27 15:11:56 -0300

    Retorno de objete em funcions

---
+++ Aula_05/function_retorne.js
@@ -0,0 +1,34 @@
+function mayFunc ()
+{
+    return ["Danilo", "Da", "Silva", "Borges"];
+}
+
+console.log(mayFunc()[0]);
+console.log(mayFunc()[1]);
+console.log(mayFunc()[0][1], mayFunc()[0][2]); // Neste ponto do código estamos acessando o primeiro arry, primeira dimensão do arry em seguida a segunda dimensão do primeiro arry
+
+// Coluna  0  1  2  3  4  5  6  7  8  9  10
+// Linha 0 D  a  n  i  l  o 
+//       1    
+//       2     
+//       3     
+//       4   
+//       5
+//       6
+//       7
+//       8
+//       9
+//       10    
+
+function mayFuncObj () 
+{
+    return pessoa = 
+    {
+        p1: 1,
+        p2: "Danilo",
+        p3: "Sobrenome",
+        p4: "Idade"
+    };
+}
+
+console.log(mayFuncObj().p1);
