c3a9baf7383852a7076d84c4aca7021902d07a22
danilo borges <danilosilva2017@hotmail.com>
2022-06-29 13:18:32 -0300

    Realizando exercícios Challenge 8

--- Aula_08/funcName.js
+++ Aula_08/funcName.js
@@ -24,3 +24,28 @@ console.log(adder(10)(5));
 
 let sum = adder(2);
 console.log(sum(3));
+
+let car =
+{
+    color: "yellow"
+};
+
+function getCarColor (mycar)
+{
+    return mycar.color;
+}
+
+console.log(getCarColor(car));
+
+
+function showOtherFunction (otherFunc)
+{
+    return otherFunc ();
+}
+
+function func ()
+{
+    return `Programação Funcional em JS Ninja!`;
+}
+
+console.log(showOtherFunction(func));  // Aqui estamos passando a intração func (Function) por parâmetro, lembrando que não podemos invocá-la
--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,26 +3,34 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+let sum = function calculateSum (x, y)
+{ 
+  return x + y;
+}
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+console.log(`A soma de 11 e 35 é igual a ${sum(11, 35)}.`)
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-// ?
+console.log(`O nome da função que faz a soma é ${sum.name}.`);
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
+function showName ()
+{
+  return this.name;
+}
+
+console.log(showName());
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
