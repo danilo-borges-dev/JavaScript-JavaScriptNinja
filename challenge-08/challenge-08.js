5f9335ce226c546faa0a298118831608f54cab76
danilo borges <danilosilva2017@hotmail.com>
2022-06-30 17:42:18 -0300

    FInalizado o desafio Challenge 8

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -27,22 +27,20 @@ seu nome.
 */
 function showName ()
 {
-  return this.name;
+  return showName.name;
 }
-
-console.log(showName());
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-// ?
+let varShowName = showName();
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-// ?
+console.log(`A função showName retorna ${varShowName}.`);
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -56,28 +54,64 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
   - Se o operador não for válido, retornar a frase:
   "Operação inválida."
 */
-// ?
 
-/*
+function calculator (operation)
+  {
+    if (operation === "+")
+    {
+      return (x, y) => x + y;
+    }
+    
+    if (operation === "-")
+    {
+      return (x, y) => x - y;
+    }
+
+    if (operation === "*")
+    {
+      return (x, y) => x * y;
+    }
+
+    if (operation === "/")
+    {
+      return (x, y) => x / y;
+    }
+
+    if (operation === "%")
+    {
+      return (x, y) => x % y;
+    }
+    
+    return "Operação inválida";
+  } 
+
+console.log(calculator("p"));
+/*,
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
 parâmetro o operador de soma.
 */
-// ?
+let sum2 = calculator("+");
 
 /*
 Agora `sum` é uma função. Mostre no console a soma de dois números, usando ela.
 */
-// ?
+console.log(sum2(7,5));
 
 /*
 Agora, declare algumas variáveis com os nomes `subtraction`, `multiplication`,
 `division` e `mod`, e atribua a elas a função `calculator`, passando o operador
 correto por parâmetro para cada uma delas.
 */
-// ?
+let subtraction2 = calculator("-");
+let multiplication = calculator("*");
+let division = calculator("/");
+let mod = calculator("%");
 
 /*
 Faça uma operação com cada uma das funções criadas acima, mostrando o resultado
 no console.
 */
-// ?
+console.log(subtraction2(7,5));
+console.log(multiplication(7,5));
+console.log(division(7,5));
+console.log(mod(7,5));
