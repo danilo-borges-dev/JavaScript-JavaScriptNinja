124f34d26935266b9b8f28e07b8defe595c7ecac
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-08 14:47:16 -0300

    Entendendo o funcionamento do splice

--- Aula_13/aula.js
+++ Aula_13/aula.js
@@ -20,3 +20,35 @@ function newFunction() {
 
     console.log(myArray_01);
 }
+
+console.log();
+
+let nameList = [ "Danilo", "João", "Maria", "Barbára"]
+
+let newList = nameList.slice(1, 2);
+console.log(newList);
+
+console.log();
+
+let newArray_03 = [1, 2, 3, 4, 5]
+
+let resultSplace = newArray_03.splice(4);
+
+console.log(resultSplace);
+
+let newArray_04 = [1, 2, 3, 4, 5]
+
+console.log(newArray_04.splice(2, 2));
+
+newArray_04.splice(1, 0,  "Danilo");
+
+console.log(newArray_04);
+
+newArray_04.splice(2, 0, "João", "Sueli", "Rosana");
+
+console.log(newArray_04);
+
+let arrayTest = newArray_04.splice(0, 1, "Marcos", "Michel");
+
+console.log(arrayTest);
+console.log(newArray_04);
