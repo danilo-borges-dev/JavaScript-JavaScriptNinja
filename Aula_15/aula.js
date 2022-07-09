0ccc13cf88a82e8b86d5221ce8aaf93a531dba35
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-09 09:12:58 -0300

    Entendo o funcionamento do every e do método some

--- Aula_15/aula.js
+++ Aula_15/aula.js
@@ -5,3 +5,13 @@ array.forEach(element => {
 });
 
 array.forEach((item, index, array) => console.log(item, index, array))
+
+console.log();
+
+let everyVar = array.every((item) => item < 5);
+console.log(everyVar);  // Retorna true ou false se todos os valores do array forem menores que 5
+
+console.log();
+
+let someVar = array.some((item) => item > 5);
+console.log(sameVar);
