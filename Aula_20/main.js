68a047b7af96c4d672b8cfb63e6e939e63424274
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-14 09:50:11 -0300

    Praticando a criação de heranca de objetos

--- Aula_20/main.js
+++ Aula_20/main.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     "use strict";
     let name = "Danilo ";
 
@@ -17,4 +17,18 @@
 
     let sliceTest = "Danilo";
     console.log(sliceTest.slice(1, 4));
+
+    let obj_01 =
+    {
+        prop_01: 1,
+        prop_02: "Danilo"
+    }
+
+    let obj_02 = Object.create(obj_01);
+
+    obj_02.prop_03 = "Nova Propriedade";
+
+    for (let item in obj_02) {
+        console.log(obj_02.hasOwnProperty(item), item);
+    }
 })();
