2e2f562858ff4dc40e5fec30fbb27b0a9ed0a595
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-14 09:42:04 -0300

    Realizando a revisao dos conteudos estudados

--- Aula_20/main.js
+++ Aula_20/main.js
@@ -7,4 +7,14 @@
     let frase = "Coragem ";
     let frase2 = "Mentalização ";
     console.log(name.concat(frase, frase2));
+    frase = "Coragem ";
+    console.log(frase.indexOf("a"));
+
+    console.log(frase.lastIndexOf("e"));
+
+    let newFrase = "Danilo da Silva Borges";
+    console.log(newFrase.replace("o", "u"));
+
+    let sliceTest = "Danilo";
+    console.log(sliceTest.slice(1, 4));
 })();
