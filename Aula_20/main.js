188233db07366d071ca09382b9fc1bf7a859d648
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-28 10:11:51 -0300

    Pratica de Revisao String

--- Aula_20/main.js
+++ Aula_20/main.js
@@ -1,71 +1,77 @@
 (function () {
-    "use strict";
-    let name = "Danilo ";
+    // "use strict";
+    // let name = "Danilo ";
 
-    console.log(name.charAt(5));
+    // console.log(name.charAt(5));
 
-    let frase = "Coragem ";
-    let frase2 = "Mentalização ";
-    console.log(name.concat(frase, frase2));
-    frase = "Coragem ";
-    console.log(frase.indexOf("a"));
+    // let frase = "Coragem ";
+    // let frase2 = "Mentalização ";
+    // console.log(name.concat(frase, frase2));
+    // frase = "Coragem ";
+    // console.log(frase.indexOf("a"));
 
-    console.log(frase.lastIndexOf("e"));
+    // console.log(frase.lastIndexOf("e"));
 
-    let newFrase = "Danilo da Silva Borges";
-    console.log(newFrase.replace("o", "u"));
+    // let newFrase = "Danilo da Silva Borges";
+    // console.log(newFrase.replace("o", "u"));
 
-    let sliceTest = "Danilo";
-    console.log(sliceTest.slice(1, 4));
+    // let sliceTest = "Danilo";
+    // console.log(sliceTest.slice(1, 4));
 
-    let obj_01 =
-    {
-        prop_01: 1,
-        prop_02: "Danilo"
-    }
+    // let obj_01 =
+    // {
+    //     prop_01: 1,
+    //     prop_02: "Danilo"
+    // }
 
-    let obj_02 = Object.create(obj_01);
+    // let obj_02 = Object.create(obj_01);
 
-    obj_02.prop_03 = "Nova Propriedade";
+    // obj_02.prop_03 = "Nova Propriedade";
 
-    for (let item in obj_02) {
-        console.log(obj_02.hasOwnProperty(item), item);
-    }
+    // for (let item in obj_02) {
+    //     console.log(obj_02.hasOwnProperty(item), item);
+    // }
 
-    let objToArray =
-    {
-        name: "Danilo",
-        age: 29,
-        sex: 'M'
-    }
+    // let objToArray =
+    // {
+    //     name: "Danilo",
+    //     age: 29,
+    //     sex: 'M'
+    // }
 
-    let newArry = Object.keys(objToArray);
+    // let newArry = Object.keys(objToArray);
 
-    console.log(newArry);
+    // console.log(newArry);
 
-    let n = "Danilo da Sivla Borges";
-    console.log(n.split());
-    console.log(n.split('n'));
+    // let n = "Danilo da Sivla Borges";
+    // console.log(n.split());
+    // console.log(n.split('n'));
 
-    let s = "Sucess";
-    console.log(s.split('s').join('d'));
+    // let s = "Sucess";
+    // console.log(s.split('s').join('d'));
 
 
-    let frase_02 = "Mensagem do dia. Seja corajoso e tenha zelo por aquilo que já possui";
+    // let frase_02 = "Mensagem do dia. Seja corajoso e tenha zelo por aquilo que já possui";
 
-    let testUpper = frase_02.toUpperCase();
-    console.log(testUpper);
+    // let testUpper = frase_02.toUpperCase();
+    // console.log(testUpper);
 
-    let testLower = testUpper.toLowerCase();
-    console.log(testLower);
+    // let testLower = testUpper.toLowerCase();
+    // console.log(testLower);
 
-    let meuNome = "danilo";
-    console.log(meuNome);
-    let meuNovoNome = meuNome.charAt(0).toLocaleUpperCase() + meuNome.slice(1);
-    console.log(meuNovoNome);
+    // let meuNome = "danilo";
+    // console.log(meuNome);
+    // let meuNovoNome = meuNome.charAt(0).toLocaleUpperCase() + meuNome.slice(1);
+    // console.log(meuNovoNome);
 
-    console.log();
+    // console.log();
 
-    console.log("meu Nome".toLocaleUpperCase());
+    // console.log("meu Nome".toLocaleUpperCase());
 
+
+    let num = "10";
+    let num2 = parseInt(num);
+    console.log(num2);
+    console.log(typeof(num));
+    console.log(typeof(num2));
 })();
