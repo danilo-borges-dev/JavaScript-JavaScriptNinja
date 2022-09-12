a0d9daa72b0a7dc19b017140982ec4ff2e897e0e
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-12 13:15:33 -0300

    Finalizado o Desafio da semana 02

--- challenge-02/app.js
+++ challenge-02/app.js
@@ -3,7 +3,7 @@
 
 
 // Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
-function fSoma (arg01, arg02){
+function fSoma(arg01, arg02) {
     return arg01 + arg02;
 }
 
@@ -21,7 +21,7 @@ Crie uma função que adicione um valor à variável criada acima, e retorne a s
     O valor da variável agora é VALOR.
 Onde VALOR é o novo valor da variável.
 */
-function adicionaValor (n1){
+function adicionaValor(n1) {
     return console.log(`O valor da variável agora é ${semValor + n1}`);
 }
 
@@ -38,19 +38,19 @@ Crie uma função com as seguintes características:
     Preencha todos os valores corretamente!
 3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
 */
-function myFunc (x1, x2, x3){
+function myFunc(x1, x2, x3) {
     let result = 0;
-    if(x1 == undefined || x2 == undefined || x3 == undefined){
+    if (x1 == undefined || x2 == undefined || x3 == undefined) {
         return console.log("Preencha todos os valores corretamente!");
     }
-    else{
+    else {
         result = (x1 * x2 * x3) + 2;
         return console.log(result);
     }
 }
 
 // Invoque a função criada acima, passando só dois números como argumento.
-myFunc(2,3);
+myFunc(2, 3);
 
 // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
 // Preencha todos os valores corretamente!
@@ -70,21 +70,29 @@ Crie uma função com as seguintes características:
 5. Se nenhum argumento for passado, retorne o valor booleano `false`.
 6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
 */
-function myNewFunc (x1, x2, x3) {
-    if (x2 == undefined || x3 == undefined){
+console.log();
+
+function myNewFunc(x1, x2, x3) {
+    if (x1 != undefined && (x2 == undefined && x3 == undefined)) {
         return console.log(x1);
     }
-    if (x3 == undefined){
+    if (x1 != undefined && x2 != undefined && x3 == undefined) {
         return console.log(x1 + x2);
-    }
-    if (x1 != undefined && x2 != undefined && x3 != undefined){
-        return console.log((x1 + x2) / x3);
     }
     if (x1 == undefined) {
         return console.log("false");
     }
-    return console.log("null");
+    if ((x1 != undefined && +x1) && (x2 != undefined && +x2) && (x3 != undefined && +x3)) {
+        return console.log((x1 + x2) / x3);
+    }
+    else {
+        return console.log("null");
+    }
 }
 
 // Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
 myNewFunc();
+myNewFunc(10);
+myNewFunc(10, 5);
+myNewFunc(10, 5, 3);
+myNewFunc("Danilo", 5, 3);
