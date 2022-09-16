cd245d572cdcc0d8c485be01c0661eda639a7efe
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-16 13:38:39 -0300

    Trabalhando Métodos em Objetos

--- Aula_02/app.js
+++ Aula_02/app.js
@@ -20,3 +20,44 @@ let pessoa = {
 }
 
 console.log(pessoa.parar());
+
+let myVar = function () {
+    return "Minha Função";
+}
+
+let obj = {};
+
+obj.myVar = myVar;
+
+console.log(obj.myVar);
+
+console.log();
+
+let novaPessoa = {
+    nome: "Danilo",
+    idade: 29,
+    sexo: 'M',
+    andouQuantosMetros: 0,
+    parado: "Sim",
+
+    andar: function () {
+        this.andouQuantosMetros = 10;
+        this.parado =  "Não";
+        return "Está andando";
+    },
+
+    parar: function () {
+        this.parado = "Sim";
+        return "Pessoa parou";
+    },
+
+    aniversario: function () {
+        return this.idade++;
+    }
+}
+
+console.log(novaPessoa.andar());  // Está andando
+console.log("Está parado? " + novaPessoa.parado);  // Não
+console.log("Idade atual " + novaPessoa.idade); // 29 anos
+novaPessoa.aniversario();  
+console.log("Nova Idade " + novaPessoa.idade);  // 30 anos
