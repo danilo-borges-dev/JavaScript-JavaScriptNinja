b93237b58c82e1e83f2a4d0c7fb4bc0261b18f12
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-16 11:24:15 -0300

    Finalizado a revisão do challenge 1

--- Aula_01/app.js
+++ Aula_01/app.js
@@ -12,3 +12,23 @@ console.log(retornoDeUmaFuncao);
 let number = new Number();
 
 console.log(typeof number);
+
+let f1 = function () {
+    return "Função anônima e sem parâmetro de entrada.";
+}
+
+function calcularSoma () {
+    return 2 + 3; // Funçao nomeada com retorno de uma operação Aritmética
+}
+
+function multiplicacao ( x ) {
+    return function ( y ) {
+        return x * y;
+    }
+}
+
+console.log((1 > 0 || 10 < 5) && !(8 != 10));
+
+let n1 = "true";
+
+if (n1 === true) { console.log("Entrou dentro do if")};
