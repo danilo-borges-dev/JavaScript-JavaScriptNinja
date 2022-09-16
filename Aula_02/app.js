f1dab4139b240fe6ae16b19b10cb55e98c113dfe
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-16 11:47:13 -0300

    Revisao de Objetos

--- Aula_02/app.js
+++ Aula_02/app.js
@@ -6,3 +6,17 @@ let person = {
 }
 
 console.log(person["nome"]);
+
+
+let pessoa = {
+    nome: "Danilo",
+    idade: 29,
+    andar: function () {
+    return "Está andando!";
+    },
+    parar: function () {
+        return "Está parado!";
+    },
+}
+
+console.log(pessoa.parar());
