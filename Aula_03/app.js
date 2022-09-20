9bc6103827ab282987937f68edbf782c035800b4
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-20 13:50:01 -0300

    Iniciada a Aula sobre Operador Ternário

---
+++ Aula_03/app.js
@@ -0,0 +1,16 @@
+// // Variavél que armazena a quantidade de vezes que um cliente comprou
+// let quantasVezesClienteComprou = 55;  
+
+// // Verifica de forma bem simples se a quantidade de vezes é diferente de zero
+// // Ou seja, se já ouve alguma compra
+// console.log(!!quantasVezesClienteComprou);
+
+let qtdHomens = 0;
+let qtdMulheres = 0;
+
+let sexo = 'M';
+
+sexo === 'M' ? qtdHomens += 1 : qtdMulheres += 1;
+
+console.log(qtdHomens);
+console.log(qtdMulheres);
