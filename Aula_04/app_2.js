9248ad013731f785b106937568a68123e2377050
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-10-19 07:25:26 -0300

    Revisao

--- Aula_04/app_2.js
+++ Aula_04/app_2.js
@@ -2,4 +2,19 @@
     "use strict";
     let nome = "Danilo";
     console.log(nome);
+
+
+    let sexo = 'M';
+    let quantidadeDeHomens = 0;
+    let quantidadeDeMulheres = 0;
+
+    sexo === 'M' ? quantidadeDeHomens++ : quantidadeDeMulheres++;
+    sexo === 'M' ? quantidadeDeHomens++ : quantidadeDeMulheres++;
+    console.log(quantidadeDeHomens);
+    console.log(quantidadeDeMulheres);
+    sexo === 'F' ? quantidadeDeHomens++ : quantidadeDeMulheres++;
+    console.log(quantidadeDeMulheres);
+
 })();
+
+
