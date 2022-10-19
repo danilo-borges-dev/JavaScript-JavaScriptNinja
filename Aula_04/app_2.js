98b36fddf3b61c8f0b9b5b72960d58fe9c6ddbf8
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-10-19 07:28:58 -0300

    Revisao de Thrufy e Falsy

--- Aula_04/app_2.js
+++ Aula_04/app_2.js
@@ -15,6 +15,18 @@
     sexo === 'F' ? quantidadeDeHomens++ : quantidadeDeMulheres++;
     console.log(quantidadeDeMulheres);
 
+    console.log(!!0);
+    console.log(!!1);
+    console.log(!!undefined);
+    console.log(!![]);
+    console.log(!!-0);
+    console.log(!!NaN);
+    console.log(!!false);
+    console.log(!!'');
+    console.log(!!"");
+    console.log(!!null);
+    console.log(!!" ");
+    console.log(!!function(){});
 })();
 
 
