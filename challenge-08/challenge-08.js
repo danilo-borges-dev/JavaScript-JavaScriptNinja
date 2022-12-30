e4e910ed8eaa0189a9205fcb880f01b04b98edbb
danilo borges <danilosilva2017@hotmail.com>
2022-12-30 07:06:10 -0300

    Resolvendo exercicios challenge 08.

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -1,75 +1,86 @@
-/*
-Declare uma variável chamada `sum` e atribua a ela uma função chamada
-`calculateSum`. A função deve receber dois parâmetros e retornar a soma
-desses parâmetros.
-*/
-// ?
+(function(){
+  /*
+  Declare uma variável chamada `sum` e atribua a ela uma função chamada
+  `calculateSum`. A função deve receber dois parâmetros e retornar a soma
+  desses parâmetros.
+  */
+  let sum = function calculateSum (paran1, paran2) {
+    return paran1 + paran2;
+  }
 
-/*
-Invoque a função criada acima, passando dois números que serão somados, e mostre
-o resultado no console, com a frase:
-"A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
-*/
-// ?
+  /*
+  Invoque a função criada acima, passando dois números que serão somados, e mostre
+  o resultado no console, com a frase:
+  "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
+  */
+  console.log(`A soma de 10 e 100 é igual a ${sum(10, 100)}.`);
 
-/*
-Mostre no console o nome da função criada acima, com a frase:
-"O nome da função que faz a soma é [NOME DA FUNÇÃO]."
-*/
-// ?
+  /*
+  Mostre no console o nome da função criada acima, com a frase:
+  "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
+  */
+  console.log(sum.name); // calculateSum
 
-/*
-Crie uma função literal chamada `showName`. Essa função deve retornar o
-seu nome.
-*/
-// ?
+  /*
+  Crie uma função literal chamada `showName`. Essa função deve retornar o
+  seu nome.
+  */
+  function showName () {
+    return showName.name;
+  }
+  console.log(showName());
 
-/*
-Declare uma variável chamada `varShowName` que recebe a função criada acima.
-*/
-// ?
+  /*
+  Declare uma variável chamada `varShowName` que recebe a função criada acima.
+  */
+  let varShowName = showName();
 
-/*
-Usando a variável criada acima, mostre no console o nome e o retorno da função
-atribuída a ela, com a seguinte frase:
-"A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
-*/
-// ?
+  /*
+  Usando a variável criada acima, mostre no console o nome e o retorno da função
+  atribuída a ela, com a seguinte frase:
+  "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
+  */
+  console.log(varShowName);
 
-/*
-Crie uma função literal chamada `calculator`, que funcione assim:
-- A função deve receber um parâmetro que dirá qual operação matemática ela
-vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
-- Essa função deve retornar uma segunda função que fará o seguinte:
-  - Essa segunda função deve receber dois parâmetros;
-  - Esses dois parâmetros serão os operandos usados na operação matemática;
-  - O retorno dessa segunda função é a operação matemática completa, com a frase:
-  "Resultado da operação: [NUMERO1] [OPERADOR] [NUMERO2] = [RESULTADO]."
-  - Se o operador não for válido, retornar a frase:
-  "Operação inválida."
-*/
-// ?
+  /*
+  Crie uma função literal chamada `calculator`, que funcione assim:
+  - A função deve receber um parâmetro que dirá qual operação matemática ela
+  vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
+  - Essa função deve retornar uma segunda função que fará o seguinte:
+    - Essa segunda função deve receber dois parâmetros;
+    - Esses dois parâmetros serão os operandos usados na operação matemática;
+    - O retorno dessa segunda função é a operação matemática completa, com a frase:
+    "Resultado da operação: [NUMERO1] [OPERADOR] [NUMERO2] = [RESULTADO]."
+    - Se o operador não for válido, retornar a frase:
+    "Operação inválida."
+  */
+  function calculator (operator) {
+    return function (x, y) {
+      return `Resultado da operação: [NUMERO1] [OPERADOR] [NUMERO2] = [RESULTADO].`;
+    }
+  } 
 
-/*
-Declare uma variável chamada `sum`, que receberá a função acima, passando como
-parâmetro o operador de soma.
-*/
-// ?
+  /*
+  Declare uma variável chamada `sum`, que receberá a função acima, passando como
+  parâmetro o operador de soma.
+  */
+  // ?
 
-/*
-Agora `sum` é uma função. Mostre no console a soma de dois números, usando ela.
-*/
-// ?
+  /*
+  Agora `sum` é uma função. Mostre no console a soma de dois números, usando ela.
+  */
+  // ?
 
-/*
-Agora, declare algumas variáveis com os nomes `subtraction`, `multiplication`,
-`division` e `mod`, e atribua a elas a função `calculator`, passando o operador
-correto por parâmetro para cada uma delas.
-*/
-// ?
+  /*
+  Agora, declare algumas variáveis com os nomes `subtraction`, `multiplication`,
+  `division` e `mod`, e atribua a elas a função `calculator`, passando o operador
+  correto por parâmetro para cada uma delas.
+  */
+  // ?
 
-/*
-Faça uma operação com cada uma das funções criadas acima, mostrando o resultado
-no console.
-*/
-// ?
+  /*
+  Faça uma operação com cada uma das funções criadas acima, mostrando o resultado
+  no console.
+  */
+  // ?
+})();
