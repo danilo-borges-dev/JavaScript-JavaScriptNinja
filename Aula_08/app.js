868f2af9c1ead28b801f563ede06c58c74417fb1
danilo borges <danilosilva2017@hotmail.com>
2022-12-30 06:29:44 -0300

    Funcoes em JS tem o memso Poder que Objetos em JS

---
+++ Aula_08/app.js
@@ -0,0 +1,8 @@
+function funcTest () {
+    return "hi";
+}
+
+console.log(funcTest.name);
+
+// Funções são Objetos de Primeira Classe - Funções tem o mesmo Tratamento que Objetos em JS - Tudo o que conseguimos fazer com Objetos também 
+// vamos conseguir fazer com funções.
