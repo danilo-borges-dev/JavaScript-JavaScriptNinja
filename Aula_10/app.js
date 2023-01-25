b001c8a648625b9c5df77b77d9cd8c1cde3458f5
danilo borges <danilosilva2017@hotmail.com>
2023-01-25 05:53:02 -0300

    Desafio Challenge 9 revisado com sucesso.

--- Aula_10/app.js
+++ Aula_10/app.js
@@ -1,7 +1,29 @@
-let name = new String("Danilo");
-let age = new Number(30);
-let ninja = new Boolean(true);
-
-
+let name = new String("Danilo Borges");
 console.log(name);
-console.log(name.valueOf());
+console.log(name.valueOf()); // Saída no Console "Danilo".
+
+
+console.log(typeof(function(){}));
+
+
+function sum (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (n1, n2){
+    if(+n1 && +n2){
+        return console.log(n1 - n2);
+    }
+    return console.log("Informe apenas números");
+}
+
+console.log(sum(10,2));
+
+console.log(typeof(sum()));
+
+console.log(sum("Ls", 10));
+
+console.log(subtract("Gc", 15));
+
+subtract(110,5)
+subtract("j", 5)
